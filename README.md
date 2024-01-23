fa3b86631a0e35745b43b3614c40e866ca544d28
Axel Libori Roch <71702817+alibori@users.noreply.github.com>
2024-01-23 15:30:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Laravel 10 API skeleton template
+
+Basic **Laravel 10** basic API skeleton template with few packages installed to use as a starting point.
+
+## Features
+
+- Prepared for API versioning.
+- `Src` namespace declared for a DDD approach.
+
+## Included packages
+
+Take a look at the `composer.json` file to see the full list of packages included.
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Schema::defaultStringLength(length: 191);
+    }
+}
